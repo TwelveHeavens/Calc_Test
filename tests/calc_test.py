ed079a232bffba1224136b743a1bb3814bb0b71a
Cybersport <Cybersport@26kadr.internal>
2025-03-27 16:47:48 +0300

    second commit

--- tests/calc_test.py
+++ tests/calc_test.py
@@ -4,8 +4,12 @@ calc = Calculator()
 
 def test_sum():
     assert calc.sum(32,55) == 87
-    assert calc.sum(7,4) == 11
+    assert calc.sum(7.7,4) == 11.7
 
 def test_multiply():
     assert calc.multiply(5,7) == 35
-    assert calc.multiply(32,4) == 128
+    assert calc.multiply(3.2,4) == 12.8
+
+def test_divide():
+    assert calc.divide(276,6) == 46
+    assert calc.divide(1192.4,22) == 54.2
